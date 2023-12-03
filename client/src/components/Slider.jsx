319bec2df5e21401dce0ca4c96bb5c0bac321f76
KadaGuetouache <wail@disroot.org>
2023-12-03 21:23:15 +0100

    Update slider
    Add multiple slides with info and image
    make slider functional
    make slider responsive

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -1,25 +1,47 @@
 import React from "react";
 import "../styles/slider.scss";
+import { slides } from "../data";
+import { useState } from "react";
 
 const Slider = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const updateIndex = (newIndex) => {
+    if (newIndex < 0) {
+      newIndex = slides.length - 1;
+    } else if (newIndex >= slides.length) {
+      newIndex = 0;
+    }
+    setActiveIndex(newIndex);
+  };
+
   return (
     <div className="slider-container">
       <div>
-        <div className="slides">
-          <div className="slide">
-            <div className="content-container">
-              <div className="info-container">
-                <h3>Lorem ipsum dolor sit amet</h3>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur, minima quia, a quae
-                </p>
-                <button>show now</button>
+        <div
+          className="slides"
+          style={{ transform: `translateX( -${activeIndex * 100}vw )` }}
+        >
+          {slides.map((slide) => (
+            <div className="slide" key={slide.id}>
+              <div
+                className="content-container"
+                style={{
+                  background: `url(${slide.img})`,
+                  backgroundSize: "cover",
+                }}
+              >
+                <div className="info-container">
+                  <h3>{slide.title}</h3>
+                  <p>{slide.description}</p>
+                  <button>show now</button>
+                </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
-      <div className="arrow left">
+      <div className="arrow left" onClick={() => updateIndex(activeIndex - 1)}>
         <svg
           width="150"
           height="174"
@@ -31,7 +53,7 @@ const Slider = () => {
           <path d="M0 87L150 0.397461V173.603L0 87Z" fill="white" />
         </svg>
       </div>
-      <div className="arrow right">
+      <div className="arrow right" onClick={() => updateIndex(activeIndex + 1)}>
         <svg
           width="150"
           height="174"
