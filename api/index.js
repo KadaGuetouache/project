cdf46d3c42a52da40f9fc6cd1530f5f1ff584242
KadaGuetouache <wail@disroot.org>
2023-12-08 17:34:51 +0100

    Connect mongodb to api

--- api/index.js
+++ api/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const userRoute = require("./routes/user");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-const PORT = 5000
+dotenv.config();
+
 const app = express();
+
+mongoose.connect(process.env.MONGO_URL)
+	.then(() => console.log("Databata Connected"))
+	.catch((error) => console.log(error))
 
 app.use(express.json())
 app.use("/api/user", userRoute);
 
-app.listen(PORT, () => console.log(`Server is running en port ${PORT}`))
+app.listen(process.env.PORT || 5500, () => console.log(`Server is running en port ${process.env.PORT}`))
