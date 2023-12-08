211908af1fcc6e3132604989fffd3078b21ccfd9
KadaGuetouache <wail@disroot.org>
2023-12-08 20:30:42 +0100

    Add password encryption to register api

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User.js");
+const cryptoJS = require("crypto-js");
 
 router.post("/register", async (req, res) => { 
 	const newUser = new User( { 
@@ -7,7 +8,7 @@ router.post("/register", async (req, res) => {
 		firstname: req.body.firstname,
 		lastname: req.body.lastname,
 		email: req.body.email,
-		password: req.body.password,
+		password: cryptoJS.AES.encrypt(req.body.password, process.env.PASS_TOKEN).toString(),
 	} )
 
 	try{ 
