139f8ed37ed28e556514ce1aa3b3aa73cdf73334
KadaGuetouache <wail@disroot.org>
2023-12-08 20:46:10 +0100

    Add jsonwebtoken to login api

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User.js");
 const cryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => { 
 	const newUser = new User( { 
@@ -28,9 +29,14 @@ router.post("/login", async (req, res) => {
 
 		user ? userPassword = cryptoJS.AES.decrypt(user.password, process.env.PASS_TOKEN).toString(cryptoJS.enc.Utf8) : null
 
+		const accessToken = jwt.sign( { 
+			id: user._id,
+			isAmdin: user.isAdmin
+		}, process.env.JWT_SECRET, { expiresIn: '3d' } )
+
 		if (user && req.body.password === userPassword) { 
 			const { password, ...others } = user._doc;
-			res.status(200).json({ ...others } );
+			res.status(200).json({ ...others, accessToken } );
 		} else { 
 			res.status(400).json("Unauthorized User: please make sure that your credentials are correct and try again!");
 		}
