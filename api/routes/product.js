6afe293f33227135687a180b444620a14fad30a7
KadaGuetouache <wail@disroot.org>
2023-12-11 15:20:13 +0100

    Update get all products route
    make route get products based en categories
    make route get products based en latest added

--- api/routes/product.js
+++ api/routes/product.js
@@ -16,9 +16,19 @@ router.post( "/", verifyTokenAndAdmin, async ( req, res ) => {
 
 // Get all products
 router.get( "/", async ( req, res ) => { 
+	const qNew = req.query.new;
+	const qCategory = req.query.category;
+
 	try{  
 		let products;
-		products = await Product.find(  );
+
+		if ( qNew ) { 
+			products = await Product.find(  ).sort( { createdAt: -1 } ).limit( 1 );
+		} else if ( qCategory ) { 
+			products = await Product.find( { categories: { $in: [ qCategory ] } } )
+		} else {
+			products = await Product.find(  );
+		}
 		res.status( 200 ).json( products )
 	} catch ( error ) { 
 		res.status( 500 ).json( error );
